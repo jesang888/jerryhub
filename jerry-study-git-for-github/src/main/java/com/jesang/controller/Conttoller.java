735287b69041dd842057498b57285e03f9ff8e1d
jerry <1623642998@qq.com>
2022-07-21 14:21:52 +0800

    创建了java项目

---
+++ jerry-study-git-for-github/src/main/java/com/jesang/controller/Conttoller.java
@@ -0,0 +1,7 @@
+package com.jesang.controller;
+
+public class Conttoller {
+    public static void main(String[] args) {
+        System.out.println("hello 我是第一波测试用例");
+    }
+}
---
+++ jerry-study-git-for-github/src/main/java/com/jesang/dao/User.java
@@ -0,0 +1,7 @@
+package com.jesang.dao;
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("我i也是第一波");
+    }
+}
